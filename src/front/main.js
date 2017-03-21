b1c09b42f5494e6c96020ba7b8807a41a9b0e6c7
huhai <huhai007@gmail.com>
2017-03-21 22:46:11 +0800

    complete

--- src/front/main.js
+++ src/front/main.js
@@ -4,7 +4,7 @@
 var SocketClient=require("socket.io-client");
 var $=require("jquery");
 
-var socket = SocketClient.connect('http://localhost:8000');
+var socket = SocketClient.connect('http://www.huhai.site');
 
 socket.on('initializeSuc', function (data) {
     $(".alert").html("服务器当前socket连接数:"+data.totalCount);
@@ -16,7 +16,7 @@ socket.on('orderSuc', function (data) {
     $(".alert").html("你下订单的时间是:"+data.order_time+" 您预定的是:"+data.item+" 当前连接总数:"+data.totalCount);
     // socket.emit('my other event', { my: '已经收到请求数'+(++count) });
 });
-
+s
 //$()==
 
 
